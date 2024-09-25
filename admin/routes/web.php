1547a59fc879407862317214eb68ce94cbe4ff15
rizhul.januar <rayjam66@gmail.com>
2024-09-25 15:10:12 +0700

    feat: create api auth, dashboard, image, order, product, role, user

--- admin/routes/web.php
+++ admin/routes/web.php
@@ -2,17 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('user', [App\Http\Controllers\AuthController::class, 'user']);
+
+// Admin
+Route::group([
+    'middleware' => 'scope.admin',
+], function () {
+    Route::get('chart', [App\Http\Controllers\DashboardController::class, 'chart']);
+    Route::post('upload', [App\Http\Controllers\ImageController::class, 'upload']);
+    Route::get('export', [App\Http\Controllers\OrderController::class, 'export']);
+
+    Route::apiResource('users', App\Http\Controllers\UserController::class);
+    Route::apiResource('roles', App\Http\Controllers\RoleController::class);
+    Route::apiResource('products', App\Http\Controllers\ProductController::class);
+    Route::apiResource('orders', App\Http\Controllers\OrderController::class)->only('index', 'show');
+    Route::apiResource('permissions', App\Http\Controllers\PermissionController::class)->only('index');
 });
