be1bf1af468a1ab9180ea90996c7930776f025b0
rizhul.januar <rayjam66@gmail.com>
2024-09-26 15:56:11 +0700

    feat: create email job

---
+++ email/app/Jobs/AdminAdded.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Mail\Message;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class AdminAdded implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $email;
+
+    public function __construct($email)
+    {
+        $this->email = $email;
+    }
+
+    public function handle()
+    {
+        Mail::send('admin.adminAdded', [], function (Message $message) {
+           $message->to($this->email);
+           $message->subject('You have been added to the Admin App!');
+       });
+    }
+}
--- email/app/Providers/EventServiceProvider.php
+++ email/app/Providers/EventServiceProvider.php
@@ -2,34 +2,16 @@
 
 namespace App\Providers;
 
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use App\Jobs\AdminAdded;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array<class-string, array<int, class-string>>
-     */
-    protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
-    ];
-
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        \App::bindMethod(AdminAdded::class . '@handle', fn($job) => $job->handle());
     }
-
+    
     /**
      * Determine if events and listeners should be automatically discovered.
      *
